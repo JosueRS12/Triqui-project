df16530c142cd8366899e4d8efc95b752c4083fe
Josue Rodriguez <josue.rodriguez.siatama@gmail.com>
2021-06-17 15:34:30 -0500

    create the board game

--- src/components/board.js
+++ src/components/board.js
@@ -4,19 +4,16 @@ import './board.css'
 
 function Square(props){
   return(
-    <button type="button"className="square">
-    {props.value} 
+    <button type="button" className="square">
+      {props.value} 
     </button>
   );
 }
 
-//function handleClick(board){
-  //console.log("before map"+board);
-  //board.map((element)=> { element = "z";
-    //return element;
-  //});
-  //console.log("before map"+board);
-//}
+//hooks structure
+function hooks(){
+
+}
 
 
 export default class Board extends React.Component{
@@ -27,29 +24,42 @@ export default class Board extends React.Component{
     this.board = [];
   }
   /*
-    0   1   2   3 
-    4   5   6   7 
-    8   9   10  11
+    0   1   2   
+    3   4   5   
+    6   7   8      
     
   */
   fillSquare(i){
-    for(let i = 0; i < 12; i++){
-      if (i===3||i===7||i===11)
-        this.board.push('');
-      else
-        this.board.push(<Square value={i}/>); 
+    for(let i = 0; i < 9; i++){
+      this.board.push(<Square value ='o'/>);
     }
   }
 
-          //{this.board.map((square) => square)}
   render(){
     return(
       <div className="board">
-       {this.fillBoard()}
+       {this.fillSquare()}
         <div className="fil">
-          {this.board.map((element)=> element? element : <br/>  )}     
+          {this.board.map((element, i)=> {
+            const fil = i<3? <div className="butt"> {element} </div> : null; 
+            return fil;
+        })}     
         </div>
+        <div className="fil">
+          {this.board.map((element, i)=> {
+           const fil = (i>=3 && i<6)? <div className="butt"> {element} </div> : null; 
+            return fil;
+        })}     
+        </div>
+        <div className="fil"> 
+          {this.board.map((element, i)=> {
+          const fil = (i>=6 && i<9)? <div className="butt"> {element} </div> : null; 
+          return fil;
+        })}     
+        </div>
+  
       </div>
     );
   }
+
 }
