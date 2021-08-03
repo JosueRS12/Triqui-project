e19138416a8bdcfab95e816767e4228668278cc4
Josue Rodriguez <josue.rodriguez.siatama@gmail.com>
2021-08-03 15:15:42 -0500

    the logical is functional

--- src/components/board.js
+++ src/components/board.js
@@ -13,14 +13,18 @@ function Square(props){
 
 
 //change value of squares
+let i = 1;
+console.log(i);
+i++;
 let turn = false;
 let count = 0;
-let combX = [];
-let combO = [];
+//values for player combinations
+let setCombinationsX = new Set();
+let setCombinationsO = new Set();
 //two players: X and O
 function ChangeValueSquare(id, value){ 
   var square = document.getElementById(id);
-  console.log(id);
+  //console.log(id);
   //if(count === 9)
     //count = 0;
   if(turn){
@@ -28,21 +32,20 @@ function ChangeValueSquare(id, value){
     square.disabled = true;
     turn = !turn;
     count++;
-    combX.push(id);
-    console.log('x '+id);
-    VerifyEndGame(count, combX, 'X');
+    setCombinationsX.add(id);
+    VerifyEndGame(count, setCombinationsX, 'X');
     return 'X'; 
   }else{ //movement player o => false
     square.disabled = true;
     turn = !turn;
     count++;
-    combO.push(id);
-    console.log('O '+id);
-    VerifyEndGame(count,combO, 'O');
+    setCombinationsO.add(id);
+    console.log(`o ${setCombinationsO}`);
+    //console.log('O '+id);
+    VerifyEndGame(count, setCombinationsO, 'O');
     return 'O';
   }
 }
-
 //verify winner
 //primero verificar que todos los botones esten deshabilitados, de ese modo se
 //sabe que el juego terminó: Cuando count es = 9, todos se encuentrar
@@ -52,18 +55,19 @@ function ChangeValueSquare(id, value){
 
 //winning combinations
 
-const combinations = ['0,1,2',
-                      '3,4,5',
-                      '6,7,8',
-                      '0,3,6',
-                      '1,4,7',
-                      '2,5,8',
-                      '2,4,6',
-                      '0,4,8'];
+const setWinningCombinations = new Set([
 
-const combiWin = new Set(combinations);
+                    [0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [2,4,6],
+                    [0,4,8] 
+]);
 
-const WinningCombinations = (comb) => {
+const WinningCombinations = (setPlayerMovements, player) => {
   // pasar un array con los indices
   // position
   ///////////////////////
@@ -75,28 +79,71 @@ const WinningCombinations = (comb) => {
   //   4   //   4    ///
   //     8 //6       ////  
    
-  //
+  // eslint-disable-next-line
+  Set.prototype.isSuperset = function(subset) {
+      for (var elem of subset) {
+          if (!this.has(elem)) {
+              return false;
+          }
+      }
+      return true;
+  }
 
+  //verify if the player movements are the winning combinations
+  for (let combination of setWinningCombinations){
+    if(setPlayerMovements.isSuperset(combination)){
+      return true;
+    }
+  } 
+
+  //console.log(setWinningCombinations);
+  
+  //let resultVerify = setVerify.isSuperset(setWinningCombinations); 
+  //console.log(resultVerify);
   //la combinación 1, 4, 7 no ganó
 
-  console.log('combiWin values: '+combiWin.values());
+  //console.log('combiWin values: '+combiWin.values());
 
 
   //iterando
-  combiWin.forEach((e)=>{
-    console.log(e, typeof(e));
-  });
+  //combiWin.forEach((e)=>{
+    //console.log(e, typeof(e));
+  //});
+  
+  ////solución con map e includes
+  //let condition = combinations.includes(comb.map((e)=> {
+    //console.log(e);
+    //return e
+  //}));
+  //console.log(condition);
+  //if(condition){
+    
+    //return true; 
+  //} 
+  //else
+    //return false;
 
 
-
+  //solución con set ----------------------------------------------------
   //ordenamiento in place
   //console.log('set comb: '+ combiWin.has('2,4,6'));
-  comb.sort((a,b)=>{
-    return a - b;
-  });
+  //console.log([...combinationWinner]);
+  //comb.sort((a,b)=>{
+    //return a - b;
+  //});
+  //console.log(playerMovements+" "+player);
+  //const test = new Set([0,1,2]);
+  //console.log('test '+typeof(test));
+  //console.log('type combination winner '+typeof(combinationWinner));
+  //console.log(combinationWinner);
+  //console.log('has '+combinationWinner.isSuperset(test));
 
-  console.log('comb ordened: '+comb);
-  return combiWin.has(comb.toString());
+  //console.log('comb ordened: '+comb);
+  //console.log(combinationWinner.has(playerMovements));
+  //return combinationWinner.has(playerMovements);
+  //-----------------------------------------------------------------------
+
+
   //combinations.map((currentValue, index)=>{
     ////console.log(comb)
     ////comb => array con las combinaciones
@@ -108,11 +155,11 @@ const WinningCombinations = (comb) => {
 }
 
 //verify end game
-function VerifyEndGame(option, comb, player){
-  if(option === 9){
+function VerifyEndGame(moves, setPlayerMovements, player){
+  if(moves === 10){
     alert('limite de movimientos');
   }
-  else if(WinningCombinations(comb)){
+  else if(WinningCombinations(setPlayerMovements, player)){
     alert(`ganó el jugador ${player}`);
   }
 }
@@ -146,8 +193,10 @@ export default class Board extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      board: []
+      board: [],
+      value: 0
     };
+    this.resetBoard =  this.resetBoard.bind(this);
   }
   /*
     0   1   2   
@@ -162,18 +211,43 @@ export default class Board extends React.Component{
     }
   }
 
-  //resetBoard(){
-    //this.board.splice(0);
-  //}
+  componentDidUpdate(){
+    if(this.state.board.length>9){
+      this.resetBoard();
+
+    }
+  }
+
+  resetBoard(){ 
+    this.setState({
+      board: [],
+      value: this.state.value+1
+    });
+
+    turn = false;
+    count = 0;
+    setCombinationsO = new Set();
+    setCombinationsX = new Set();
+
+    //combX = [];
+    //combO = [];
+    //this.state.board.forEach((e)=>{
+      //console.log(e, typeof(e));
+    //});
+    //this.state.board.splice(0);
+    //this.state.board.forEach((e)=>{
+      //console.log(e, typeof(e));
+    //});
+  }
   
   
   render(){
+    this.fillSquare();
     return(
-      <div className="board">
+      <div className="board" key={this.state.value}>
       <h1>using hooks</h1>
       <HowToUseHooks/>
       <h3>Testing</h3> 
-        {this.fillSquare()}
         <div className="fil">
           {this.state.board.map((element, i)=> {
             const fil = i<3? <div className="butt"> {element} </div> : null; 
@@ -192,7 +266,9 @@ export default class Board extends React.Component{
           return fil;
         })}     
         </div> 
-        <button type="button" className="resetButton" onClick={this.resetBoard}> ResetGame </button>
+        <div> 
+          <button type="button" className="resetButton" onClick={this.resetBoard}> ResetGame </button>
+        </div>
       </div>
     );
   }
