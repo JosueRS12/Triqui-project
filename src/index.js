908a4c14408862538294c557e764c82a61ccd31b
Josue Rodriguez <josue.rodriguez.siatama@gmail.com>
2021-08-24 16:06:56 -0500

    footer with logos

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 //vincular el css
 import './index.css';
 import Board from './components/game/board';
+import logoGlud from './assets/GLUD_LOGO.svg';
 //vincular boostrap
-//import Navbar from 'react-bootstrap/Navbar';
-//import Container from 'react-bootstrap/Container'
-//import Nav from 'react-bootstrap/Nav';
-//import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import {Col, Row, Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 const Layout = (props) =>{
+  //function redes(name){
+    //let redSocial = {link: '', class: ''};
+//// eslint-disable-next-line
+    //switch (name) {
+      //case 'github':
+        //redSocial.link = 'https://github.com/JosueRS12/';    
+        //redSocial.class = 'github';
+        //break;
+      //case 'linkedin':
+        //redSocial.link = 'https://www.linkedin.com/in/josu%C3%A9-rodr%C3%ADguez-720095218';    
+        //redSocial.class = 'linkedin';
+        //break;
+    //} 
+    //return redSocial;
+  //} 
   return (
     <main>
       <Navbar bg="light" expand="lg">
@@ -20,10 +32,11 @@ const Layout = (props) =>{
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Repositorio</Nav.Link>
+            <Nav.Link href="https://github.com/JosueRS12/Triqui-project" target="_blank">Repositorio</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#home">redes sociales</Nav.Link>
+             
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -32,9 +45,21 @@ const Layout = (props) =>{
         <Board/>
       </section>
       
-      <section id="footer">
-        <p>hola</p>
-      </section>
+      <footer>
+        <Container>
+          <Row>
+            <Col className="align">
+              <div className="container-img">
+                <img src={logoGlud} alt="logo glud"/>
+              </div>
+            </Col>
+            <Col className="align social">
+              <a href="https://github.com/JosueRS12/" className="social github"></a>
+              <a href="https://www.linkedin.com/in/josu%C3%A9-rodr%C3%ADguez-720095218" className="social linkedin"></a>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </main>
     
   );
